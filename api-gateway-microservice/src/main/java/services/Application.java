6664cfd0ce9f476628b3e9224a1445421b99a3a7
michalbliznak <michal.bliznak@gmail.com>
2019-05-11 21:26:11 +0200

    - Add initial support for OAuth2 authorization (working with Github).

--- api-gateway-microservice/src/main/java/services/Application.java
+++ api-gateway-microservice/src/main/java/services/Application.java
@@ -2,11 +2,13 @@ package services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableOAuth2Sso
 @EnableZuulProxy
 public class Application {
 	public static void main(String[] args) {
