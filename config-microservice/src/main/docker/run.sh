8b2f8287c2b90b48c2164e3d29c49c9363ff6156
michalbliznak <michal.bliznak@gmail.com>
2019-05-13 04:38:12 +0200

    - Initial support for centralized configuration

---
+++ config-microservice/src/main/docker/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "********************************************************"
+echo "Waiting for services to start on ports $WAIT_FOR_PORTS"
+echo "********************************************************"
+IFS=',' read -ra services <<< "${WAIT_FOR_SERVICES}"
+for srv in "${services[@]}"; do
+    echo "Waiting for service ${srv}..."
+    url=`echo ${srv} | cut -f1 -d:`
+    port=`echo ${srv} | cut -f2 -d:`
+    while ! `nc -z $url $port`; do sleep 3; done
+done
+echo "******* Services have started"
+
+echo "********************************************************"
+echo "Starting Configuration Server"
+echo "********************************************************"
+java -jar /usr/local/configservice/@project.build.finalName@.jar
--- config-microservice/src/main/java/services/Application.java
+++ config-microservice/src/main/java/services/Application.java
@@ -2,15 +2,10 @@ package services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-/**
- * Created by michalbliznak on 3.8.17.
- */
 @SpringBootApplication
-@EnableDiscoveryClient
-@RefreshScope
+@EnableConfigServer
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
